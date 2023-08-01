da6bc5673099471d76b6ca29260b7ca1c57ee72b
Oleksii Khatuntsev <oleksii_khatuntsev@symbotic.com>
2023-08-01 20:07:16 +0200

    Initial commit

---
+++ and_or/main.py
@@ -0,0 +1,16 @@
+#Напишіть цикл, який буде вимагати від користувача ввести слово, в якому є буква "о"
+# (враховуються як великі так і маленькі). Цикл не повинен завершитися,
+# якщо користувач ввів слово без букви "о".
+
+# user_word = input("Please enter a word with letter 'O' --> ")
+#
+# while "o" not in user_word and "O" not in user_word:
+#     user_word = input("The letter 'O' is absent in your word, please enter another one --> ")
+#
+# print(f"Yes, the letter 'О' is present in word '{user_word}'")
+
+user_input = int(input("Enter number"))
+
+        # 0 or 1           0 or 1
+if (user_input > 10) and (user_input == 5 or user_input == input("Second query")):
+    print(f"10 < {user_input} < 100")
---
+++ functions/import_example.py
@@ -0,0 +1,8 @@
+def get_summ(*args):
+    a = 0
+    return sum(args)
+
+b = [56]
+
+def test_func():
+    print(id(b))
