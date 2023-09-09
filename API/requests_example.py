6341e6f4f170994ec8e56dc2c3ecbb61c742d740
Oleksii Khatuntsev <oleksii_khatuntsev@symbotic.com>
2023-09-09 10:35:48 +0200

    add selenium

--- API/requests_example.py
+++ API/requests_example.py
@@ -1,3 +1,4 @@
+import pytest
 import requests
 
 class UserSignUpModel:
@@ -9,39 +10,25 @@ class UserSignUpModel:
         self.repeatPassword = repeat_password
 
 
+class TestRegistration:
 
-# test_dict = {
-#   "name": "John",
-#   "lasstName": "Dou",
-#   "email": "sadasdaasdsaasdasdsddas@test.com",
-#   "password": "Qwerty12345",
-#   "repeatPassword": "Qwerty12345"
-# }
+    def setup_class(self):
+        self.user_to_sign_up = UserSignUpModel("John", "Dou", "sadasdasadasdasasddasdasdsaasdasdsddas@test.com",
+                                          "Qwerty12345", "Qwerty12345")
 
+    def setup_method(self):
+        self.session = requests.session()
 
-# url = 'https://api.github.com/some/endpoint'
-# payload = {'some': 'data'}
-# headers = {'content-type': 'application/json'}
-#
-# r = requests.post(url=url, json=payload, headers=headers)
+    @pytest.mark.parametrize("user", [(UserSignUpModel("John", "Dou", "sadasdasadasdasasddasdasdsaasdasdsddas@test.com",
+                                          "Qwerty12345", "Qwerty12345")), (UserSignUpModel("John", "Dou", "sadasdasadasdasasddasdasdsaasdasdsddas@test.com",
+                                          "Qwerty12345", "Qwerty12345"))])
+    def test_check_successful_user_registration(self, user):
+        post_new_user = self.session.post(url="https://qauto2.forstudy.space/api/auth/signup", json=self.user_to_sign_up.__dict__)
+        assert post_new_user.status_code == 201
 
-user_to_sign_up = UserSignUpModel("John", "Dou", "sadasdaasdasdasdasdsaasdasdsddas@test.com", "Qwerty12345", "Qwerty12345")
+    def test_check_successful_user_registration1(self):
+        post_new_user = self.session.post(url="https://qauto2.forstudy.space/api/auth/signup", json=self.user_to_sign_up.__dict__)
+        assert post_new_user.status_code == 201
 
-session = requests.session()
-# session.headers.
-get_current_user = session.get("https://qauto2.forstudy.space/api/users/current")
-post_new_user = session.post(url="https://qauto2.forstudy.space/api/auth/signup", json=user_to_sign_up.__dict__, headers=)
-get_current_user_after_post = session.get("https://qauto2.forstudy.space/api/users/current")
-print(get_current_user.text)
-print(post_new_user.text)
-print(get_current_user_after_post.text)
-
-
-# session = requests.session()
-
-#
-# post_new_user = session.post("https://qauto2.forstudy.space/api/auth/signup", json=test_dict)
-# get_current_user_after_post = session.get("https://qauto2.forstudy.space/api/users/current")
-# print(get_current_user.text)
-# print(post_new_user.text)
-# print(get_current_user_after_post.text)
+    def teardown_method(self):
+        self.session.delete("https://qauto2.forstudy.space/api/users")
